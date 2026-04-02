88fc9888e0f4387808f16047f1b77dd95c761905
martindubos <martindubos06@gmail.com>
2026-04-02 14:55:58 +0000

    parsers

--- src/Parsers/PlayerLoader.py/FootballPlayerLoader.py
+++ src/Parsers/PlayerLoader.py/FootballPlayerLoader.py
@@ -1,3 +1,9 @@
+import csv
+
+
+import numpy as np
+import pandas as pd
 
 
 class FootballPlayerLoader():
+    
--- src/Parsers/PlayerLoader.py/TennisPlayerLoader.py
+++ src/Parsers/PlayerLoader.py/TennisPlayerLoader.py
@@ -2,3 +2,4 @@ from .PlayerLoader.TennisMenPlayerLoader import TennisMenPlayerLoader
 from .PlayerLoader.TennisWomenPlayerLoader import TennisWomenPlayerLoader
 
 class TennisPlayerLoader():
+   
